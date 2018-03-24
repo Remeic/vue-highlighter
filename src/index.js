9ceef6316e65abbbefc1ef5d08cdf763e7107ed3
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-03-24 01:05:13 +0100

    Fix update event, removed useless argument from highlight function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Vue from "vue"
 
 let originalContent = undefined
 
-function highlight(content, word, el) {
+function highlight(content, word) {
   const span = "<span style='padding:0px 5px; background-color:#009688; color:white'>"
   const spanEnd = "</span>"
   let words = content.split(" ")
@@ -18,11 +18,11 @@ export const vueHighlighter = {
   bind(el, binding, vnode) {
     originalContent = el.innerHTML
      if (binding.arg == "word") {
-      el.innerHTML = highlight(originalContent, binding.value, el)
+      el.innerHTML = highlight(originalContent, binding.value)
      }
   },
   update(el, binding, vnode, oldVnode){
-    el.innerHTML = highlight(binding.value, el)
+    el.innerHTML = highlight(el.innerText, binding.value)
   },
   unbind(el, binding, vnode){
     el.innerHTML = originalContent
