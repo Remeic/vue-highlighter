cb19cabfec1fbab9cd3b0c929e8efda637152c56
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-04-23 18:01:06 +0200

    Remove comments in test file

--- src/index.test.js
+++ src/index.test.js
@@ -3,37 +3,6 @@ import app from '../example/App'
 
 import vueHighlighter from '.';
 
-// test('it works', () => {
-
-//   const Component = {
-//     template: '<p v-highlight="{ word: word, live: live }">Hello World</p>'
-//   }
-
-//   const localVue = createLocalVue()
-
-//   localVue.directive('highlight',  vueHighlighter)
-
-//   const wrapper = shallow(Component, {
-//     localVue,
-//     data: {
-//       word: '',
-//       live: false
-//     },
-//   })
-  
-//   expect(wrapper.html()).toBe('<p>Hello World</p>')
-//   wrapper.setData({word: "World"})
-//   expect(wrapper.html()).toBe(
-//     '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:white\">World</span></p>'
-//   )
-//   wrapper.setData({word: "Hello"})
-//   expect(wrapper.html()).toBe(
-//     '<p><span style="padding:0px 5px; background-color:#009688; color:white">Hello</span> World</p>'
-//   )
-//   wrapper.setData({word: "Everyone"})
-//   expect(wrapper.html()).toBe('<p>Hello World</p>')
-// })
-
 
 describe('Default Behaviour',() => {
   const Component = {
