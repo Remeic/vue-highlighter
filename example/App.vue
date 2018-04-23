8ed71a97a274a1f4850bd5e98f319d3d9529c2e9
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-04-23 16:28:22 +0200

    Improve Example for new Major release

--- example/App.vue
+++ example/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <p v-highlight="{ word: word, live: live }" >{{ text }}</p>
-    <button @click="live = !live">Change Live value to {{ !live }}</button>
+    <p style="padding-left:1rem;" v-highlight="{ word: word, live: live }" >{{ text }}</p>
+    <fieldset style="border: none;">
+      <label for="word">Search</label>
+      <input :disabled="!live" type="text" name="word" placeholder="Your word here" v-model="word">
+      <br><br>
+      <button @click="live = !live">Change Live value to <b>{{ !live }}</b></button>
+    </fieldset>
   </div>
 </template>
 
@@ -11,7 +16,7 @@ import vueHighlighter from '../src/index'
 export default {
   data: ()=>{
     return {
-      text: 'I love Alessandra Alessandrauno',
+      text: 'I love Alessandra AlessandraAmore',
       word: 'Alessandra',
       live: false,
     }
@@ -21,3 +26,19 @@ export default {
   }
 }
 </script>
+
+<style>
+  button{
+    color: white;
+    background-color: #0288D1;
+    padding: 0.5rem 0.7rem;
+    border-radius: 4px;
+    border: none;
+  }
+  
+  #app{
+    margin-top: 4rem;
+    margin-left: 1rem;
+  }
+</style>
+
