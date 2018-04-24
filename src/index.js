d1cade341e70f55a7dbd268064adee4157b279ba
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-04-24 16:50:45 +0200

    Add logic for custom background color

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Vue from "vue"
 
 let originalContent = undefined
 const textColorDefault = '#fff'
+const bgColorDefault = '#009688'
 
 function selectPattern(word, liveHighlighting = false) {
   let result = ""
@@ -13,21 +14,36 @@ function selectPattern(word, liveHighlighting = false) {
   return result
 }
 
-function testColor(color = textColorDefault) {
+function testTextColor(color = textColorDefault){
   let result = textColorDefault
-  let isAColor = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color)
-  let isAString = /^[a-zA-Z]+$/.test(color)
-  if(isAColor || isAString){
+  if(testColor(color)){
     result = color
   }
   return result
 }
 
-function highlight(content, word, color, patternSelected) {
+function testBgColor(color = bgColorDefault){
+  let result = bgColorDefault
+  if (testColor(color)) {
+    result = color
+  }
+  return result
+}
+
+function testColor(color) {
+  let result = false
+  let isAColor = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color)
+  let isAString = /^[a-zA-Z]+$/.test(color)
+  if(isAColor || isAString){
+    result = true
+  }
+  return result
+}
+
+function highlight(content, word, color, bgColor, patternSelected) {
   const spanStart =
-    "<span style='padding:0px 5px; background-color:#009688; color:"+color+"'>"
+    "<span style='padding:0px 5px; background-color:"+bgColor+"; color:"+color+"'>"
   const spanEnd = "</span>"
-  console.log(spanStart)
   let result = content
   if (word != "") {
     let regex = new RegExp(patternSelected, "g")
@@ -42,6 +58,7 @@ const vueHighlighter = {
     let pattern = ''
     let word = ''
     let color = textColorDefault
+    let bgColor = bgColorDefault
     if (binding.value.word != undefined) {
       word = binding.value.word
     }
@@ -49,25 +66,31 @@ const vueHighlighter = {
       pattern = selectPattern(word, binding.value.live)
     }
     if(binding.value.style != undefined) {
-      color = testColor(binding.value.style.color)
-      console.log(binding.value.style.color)
+      color = testTextColor(binding.value.style.color)
     }
-    el.innerHTML = highlight(originalContent, word, color, pattern)
+    if(binding.value.style != undefined) {
+      bgColor = testBgColor(binding.value.style.bgColor)
+    }
+    el.innerHTML = highlight(originalContent, word, color, bgColor, pattern)
 
   },
   update(el, binding, vnode, oldVnode) {
     let pattern = ''
     let color = textColorDefault
+    let bgColor = bgColorDefault
     if (binding.value.style != undefined) {
-      color = testColor(binding.value.style.color)
+      color = testTextColor(binding.value.style.color)
+    }
+    if (binding.value.style != undefined) {
+      bgColor = testBgColor(binding.value.style.bgColor)
     }
     if (binding.value.live) {
       pattern = selectPattern(binding.value.word,binding.value.word)
-      el.innerHTML = highlight(el.textContent, binding.value.word, color, pattern)
+      el.innerHTML = highlight(el.textContent, binding.value.word, color, bgColor, pattern)
     }
     else{
       pattern = selectPattern(binding.value.word, binding.value.live)
-      el.innerHTML = highlight(originalContent, binding.value.word, color, pattern)
+      el.innerHTML = highlight(originalContent, binding.value.word, color, bgColor, pattern)
     }    
   },
   unbind(el, binding, vnode) {
