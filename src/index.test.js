ace15ead96c7f205f71d98fb0617ce214e0bfa05
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-04-24 16:02:47 +0200

    Replaced white with HEX color, add some test for color of text

--- src/index.test.js
+++ src/index.test.js
@@ -3,220 +3,220 @@ import { shallow, createLocalVue } from "@vue/test-utils"
 import vueHighlighter from '.';
 import sinon from 'sinon'
 
-// describe('Default Behaviour',() => {
-//   const Component = {
-//     template: '<p v-highlight="{ word: word, live: live }">Hello World</p>'
-//   }
+describe('Default Behaviour',() => {
+  const Component = {
+    template: '<p v-highlight="{ word: word, live: live }">Hello World</p>'
+  }
 
-//   const localVue = createLocalVue()
+  const localVue = createLocalVue()
 
-//   localVue.directive('highlight', vueHighlighter)
+  localVue.directive('highlight', vueHighlighter)
 
-//   const wrapper = shallow(Component, {
-//     localVue,
-//     data: {
-//       word: '',
-//       live: false
-//     },
-//   })
+  const wrapper = shallow(Component, {
+    localVue,
+    data: {
+      word: '',
+      live: false
+    },
+  })
 
-//   it('Empty word', () => {
-//     wrapper.setData({ word: '' })
-//     expect(wrapper.html()).toBe('<p>Hello World</p>')
-//   })
+  it('Empty word', () => {
+    wrapper.setData({ word: '' })
+    expect(wrapper.html()).toBe('<p>Hello World</p>')
+  })
 
-//   it('Contained word', () => {
-//     wrapper.setData({ word: 'World' })
-//     expect(wrapper.html()).toBe(
-//       '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:white\">World</span></p>'
-//     )
-//   })
+  it('Contained word', () => {
+    wrapper.setData({ word: 'World' })
+    expect(wrapper.html()).toBe(
+      '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:#fff\">World</span></p>'
+    )
+  })
 
-//   it('Not contained word', () => {
-//     wrapper.setData({ word: 'Giulio' })
-//     expect(wrapper.html()).toBe('<p>Hello World</p>')
-//   })
-// })
+  it('Not contained word', () => {
+    wrapper.setData({ word: 'Giulio' })
+    expect(wrapper.html()).toBe('<p>Hello World</p>')
+  })
+})
 
-// describe('Live Behaviour',() => {
-//   const Component = {
-//     template: '<p v-highlight="{ word: word, live: live }">Hello World</p>'
-//   }
+describe('Live Behaviour',() => {
+  const Component = {
+    template: '<p v-highlight="{ word: word, live: live }">Hello World</p>'
+  }
 
-//   const localVue = createLocalVue()
+  const localVue = createLocalVue()
 
-//   localVue.directive('highlight', vueHighlighter)
+  localVue.directive('highlight', vueHighlighter)
 
-//   const wrapper = shallow(Component, {
-//     localVue,
-//     data: {
-//       word: '',
-//       live: true
-//     },
-//   })
+  const wrapper = shallow(Component, {
+    localVue,
+    data: {
+      word: '',
+      live: true
+    },
+  })
 
-//   it('Empty word', () => {
-//     wrapper.setData({ word: '' })
-//     expect(wrapper.html()).toBe('<p>Hello World</p>')
-//   })
+  it('Empty word', () => {
+    wrapper.setData({ word: '' })
+    expect(wrapper.html()).toBe('<p>Hello World</p>')
+  })
 
-//   it('Contained word', () => {
-//     wrapper.setData({ word: 'World' })
-//     expect(wrapper.html()).toBe(
-//       '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:white\">World</span></p>'
-//     )
-//   })
+  it('Contained word', () => {
+    wrapper.setData({ word: 'World' })
+    expect(wrapper.html()).toBe(
+      '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:#fff\">World</span></p>'
+    )
+  })
 
-//   it('Not contained word', () => {
-//     wrapper.setData({ word: 'Giulio' })
-//     expect(wrapper.html()).toBe('<p>Hello World</p>')
-//   })
+  it('Not contained word', () => {
+    wrapper.setData({ word: 'Giulio' })
+    expect(wrapper.html()).toBe('<p>Hello World</p>')
+  })
 
-//   it('Contained substring', () => {
-//     wrapper.setData({ word: 'ell' })
-//     expect(wrapper.html()).toBe('<p>H<span style=\"padding:0px 5px; background-color:#009688; color:white\">ell</span>o World</p>')
-//   })
+  it('Contained substring', () => {
+    wrapper.setData({ word: 'ell' })
+    expect(wrapper.html()).toBe('<p>H<span style=\"padding:0px 5px; background-color:#009688; color:#fff\">ell</span>o World</p>')
+  })
 
-// })
+})
 
-// describe('Live Behaviour - False -> True', () => {
-//   const Component = {
-//     template: '<p v-highlight="{ word: word, live: live }">Hello World</p>'
-//   }
+describe('Live Behaviour - False -> True', () => {
+  const Component = {
+    template: '<p v-highlight="{ word: word, live: live }">Hello World</p>'
+  }
 
-//   const localVue = createLocalVue()
+  const localVue = createLocalVue()
 
-//   localVue.directive('highlight', vueHighlighter)
+  localVue.directive('highlight', vueHighlighter)
 
-//   const wrapper = shallow(Component, {
-//     localVue,
-//     data: {
-//       word: '',
-//       live: false
-//     },
-//   })
+  const wrapper = shallow(Component, {
+    localVue,
+    data: {
+      word: '',
+      live: false
+    },
+  })
 
-//   beforeEach(() => {
-//     wrapper.setData({ live: false })
-//   })
+  beforeEach(() => {
+    wrapper.setData({ live: false })
+  })
 
-//   it('Empty word', () => {
-//     wrapper.setData({ word: '' })
-//     wrapper.setData({ live: true })
-//     expect(wrapper.html()).toBe('<p>Hello World</p>')
-//   })
+  it('Empty word', () => {
+    wrapper.setData({ word: '' })
+    wrapper.setData({ live: true })
+    expect(wrapper.html()).toBe('<p>Hello World</p>')
+  })
 
-//   it('Contained word', () => {
-//     wrapper.setData({ word: 'World' })
-//     expect(wrapper.html()).toBe(
-//       '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:white\">World</span></p>'
-//     )
-//     wrapper.setData({ live: true })
-//     expect(wrapper.html()).toBe(
-//       '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:white\">World</span></p>'
-//     )
-//   })
+  it('Contained word', () => {
+    wrapper.setData({ word: 'World' })
+    expect(wrapper.html()).toBe(
+      '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:#fff\">World</span></p>'
+    )
+    wrapper.setData({ live: true })
+    expect(wrapper.html()).toBe(
+      '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:#fff\">World</span></p>'
+    )
+  })
 
-//   it('Not contained word', () => {
-//     wrapper.setData({ word: 'Giulio' })
-//     expect(wrapper.html()).toBe('<p>Hello World</p>')
-//     wrapper.setData({ live: true })
-//     expect(wrapper.html()).toBe('<p>Hello World</p>')
-//   })
+  it('Not contained word', () => {
+    wrapper.setData({ word: 'Giulio' })
+    expect(wrapper.html()).toBe('<p>Hello World</p>')
+    wrapper.setData({ live: true })
+    expect(wrapper.html()).toBe('<p>Hello World</p>')
+  })
 
-//   it('Contained substring', () => {
-//     wrapper.setData({ word: 'ell' })
-//     expect(wrapper.html()).toBe('<p>Hello World</p>')
-//     wrapper.setData({ live: true })
-//     expect(wrapper.html()).toBe('<p>H<span style=\"padding:0px 5px; background-color:#009688; color:white\">ell</span>o World</p>')
-//   })
+  it('Contained substring', () => {
+    wrapper.setData({ word: 'ell' })
+    expect(wrapper.html()).toBe('<p>Hello World</p>')
+    wrapper.setData({ live: true })
+    expect(wrapper.html()).toBe('<p>H<span style=\"padding:0px 5px; background-color:#009688; color:#fff\">ell</span>o World</p>')
+  })
 
-// })
+})
 
-// describe('Live Behaviour - True -> False', () => {
-//   const Component = {
-//     template: '<p v-highlight="{ word: word, live: live }">Hello World</p>'
-//   }
+describe('Live Behaviour - True -> False', () => {
+  const Component = {
+    template: '<p v-highlight="{ word: word, live: live }">Hello World</p>'
+  }
 
-//   const localVue = createLocalVue()
+  const localVue = createLocalVue()
 
-//   localVue.directive('highlight', vueHighlighter)
+  localVue.directive('highlight', vueHighlighter)
 
-//   const wrapper = shallow(Component, {
-//     localVue,
-//     data: {
-//       word: '',
-//       live: true
-//     },
-//   })
+  const wrapper = shallow(Component, {
+    localVue,
+    data: {
+      word: '',
+      live: true
+    },
+  })
 
-//   beforeEach(() => {
-//     wrapper.setData({ live: true })
-//   })
+  beforeEach(() => {
+    wrapper.setData({ live: true })
+  })
 
-//   it('Empty word', () => {
-//     wrapper.setData({ word: '' })
-//     wrapper.setData({ live: true })
-//     expect(wrapper.html()).toBe('<p>Hello World</p>')
-//   })
+  it('Empty word', () => {
+    wrapper.setData({ word: '' })
+    wrapper.setData({ live: true })
+    expect(wrapper.html()).toBe('<p>Hello World</p>')
+  })
 
-//   it('Contained word', () => {
-//     wrapper.setData({ word: 'World' })
-//     expect(wrapper.html()).toBe(
-//       '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:white\">World</span></p>'
-//     )
-//     wrapper.setData({ live: false })
-//     expect(wrapper.html()).toBe(
-//       '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:white\">World</span></p>'
-//     )
-//   })
+  it('Contained word', () => {
+    wrapper.setData({ word: 'World' })
+    expect(wrapper.html()).toBe(
+      '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:#fff\">World</span></p>'
+    )
+    wrapper.setData({ live: false })
+    expect(wrapper.html()).toBe(
+      '<p>Hello <span style=\"padding:0px 5px; background-color:#009688; color:#fff\">World</span></p>'
+    )
+  })
 
-//   it('Not contained word', () => {
-//     wrapper.setData({ word: 'Giulio' })
-//     expect(wrapper.html()).toBe('<p>Hello World</p>')
-//     wrapper.setData({ live: false })
-//     expect(wrapper.html()).toBe('<p>Hello World</p>')
-//   })
+  it('Not contained word', () => {
+    wrapper.setData({ word: 'Giulio' })
+    expect(wrapper.html()).toBe('<p>Hello World</p>')
+    wrapper.setData({ live: false })
+    expect(wrapper.html()).toBe('<p>Hello World</p>')
+  })
 
-//   it('Contained substring', () => {
-//     wrapper.setData({ word: 'ell' })
-//     expect(wrapper.html()).toBe('<p>H<span style=\"padding:0px 5px; background-color:#009688; color:white\">ell</span>o World</p>')
-//     wrapper.setData({ live: false })
-//     expect(wrapper.html()).toBe('<p>Hello World</p>')
-//   })
+  it('Contained substring', () => {
+    wrapper.setData({ word: 'ell' })
+    expect(wrapper.html()).toBe('<p>H<span style=\"padding:0px 5px; background-color:#009688; color:#fff\">ell</span>o World</p>')
+    wrapper.setData({ live: false })
+    expect(wrapper.html()).toBe('<p>Hello World</p>')
+  })
 
-// })
+})
 
-// describe('Unbind Directive',() => {
-//   const Component = {
-//     template: '<p v-highlight="{ word: word, live: live }">Hello World</p>'
-//   }
+describe('Unbind Directive',() => {
+  const Component = {
+    template: '<p v-highlight="{ word: word, live: live }">Hello World</p>'
+  }
 
-//   const localVue = createLocalVue()
+  const localVue = createLocalVue()
 
-//   localVue.directive('highlight', vueHighlighter)
-//   const spy = sinon.stub()
+  localVue.directive('highlight', vueHighlighter)
+  const spy = sinon.stub()
 
-//   const wrapper = shallow(Component, {
-//     localVue,
-//     data: {
-//       word: 'Hello',
-//       live: true
-//     },
-//     destroyed() {
-//       spy()
-//     }
-//   }).destroy()
+  const wrapper = shallow(Component, {
+    localVue,
+    data: {
+      word: 'Hello',
+      live: true
+    },
+    destroyed() {
+      spy()
+    }
+  }).destroy()
 
-//   it('Method called on destroy', () => {
-//     expect(spy.calledOnce).toBe(true)
-//   })
+  it('Method called on destroy', () => {
+    expect(spy.calledOnce).toBe(true)
+  })
 
-// })
+})
 
 describe('Custom color of text',() => {
   const Component = {
-    template: '<p v-highlight="{ word: word, live: live }">Hello World</p>'
+    template: '<p v-highlight="{ word: word, live: live, style: style }">Hello World</p>'
   }
 
   const localVue = createLocalVue()
@@ -235,15 +235,33 @@ describe('Custom color of text',() => {
   })
 
   it('Default color of text', () => {
+    expect(wrapper.html()).toBe('<p><span style=\"padding:0px 5px; background-color:#009688; color:#fff\">Hello</span> World</p>')
+  })
+
+  it('Custom color of text as hex', () => {
+    wrapper.setData({ 
+      style: {
+        color: '#000000'
+      } 
+    })
+    expect(wrapper.html()).toBe('<p><span style=\"padding:0px 5px; background-color:#009688; color:#000000\">Hello</span> World</p>')
+  })
+
+  it('Custom color of text as word', () => {
+    wrapper.setData({ 
+      style: {
+        color: 'white'
+      } 
+    })
     expect(wrapper.html()).toBe('<p><span style=\"padding:0px 5px; background-color:#009688; color:white\">Hello</span> World</p>')
   })
 
-  it('Custom color of text', () => {
+  it('Wrong custom color', () => {
     wrapper.setData({ 
       style: {
-        color: '#ff0000'
+        color: 'e54%'
       } 
     })
-    expect(wrapper.html()).toBe('<p><span style=\"padding:0px 5px; background-color:#009688; color:#ff0000\">Hello</span> World</p>')
+    expect(wrapper.html()).toBe('<p><span style=\"padding:0px 5px; background-color:#009688; color:#fff\">Hello</span> World</p>')
   })
 })
