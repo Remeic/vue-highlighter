b42e791379453e5e5958c4a8d495586226aeb5e4
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-04-27 11:09:05 +0200

    TDD, add other test on padding value changes

--- src/index.test.js
+++ src/index.test.js
@@ -379,69 +379,6 @@ describe('Custom color of background', () => {
   })
 })
 
-
-describe('Custom Padding', () => {
-  const Component = {
-    template: '<p v-highlight="{ word: word, live: live, style: style }">Hello World</p>'
-  }
-
-  const localVue = createLocalVue()
-
-  localVue.directive('highlight', vueHighlighter)
-
-  const wrapper = shallow(Component, {
-    localVue,
-    data: {
-      word: 'Hello',
-      live: true,
-      style: {
-        color: '',
-        bgColor: '',
-        padding:''
-      }
-    }
-  })
-
-  it('Default padding', () => {
-    expect(wrapper.html()).toBe('<p><span style=\"padding:0px 5px; background-color:#009688; color:#fff\">Hello</span> World</p>')
-  })
-
-  it('Custom padding - 1 value', () => {
-    wrapper.setData({
-      style: {
-        padding: '1px'
-      }
-    })
-    expect(wrapper.html()).toBe('<p><span style=\"padding:1px; background-color:#000000; color:#111\">Hello</span> World</p>')
-  })
-
-  it('Custom padding - 2 value', () => {
-    wrapper.setData({
-      style: {
-        padding: '1px 2px'
-      }
-    })
-    expect(wrapper.html()).toBe('<p><span style=\"padding:1px 2px; background-color:white; color:white\">Hello</span> World</p>')
-  })
-
-  it('Custom padding - 3 value', () => {
-    wrapper.setData({
-      style: {
-        padding: '1px 2px 3px'
-      }
-    })
-    expect(wrapper.html()).toBe('<p><span style=\"padding:1px 2px 3px; background-color:white; color:white\">Hello</span> World</p>')
-  })
-
-  it('Custom padding - 4 value', () => {
-    wrapper.setData({
-      style: {
-        padding: '1px 2px 3px 4px'
-      }
-    })
-    expect(wrapper.html()).toBe('<p><span style=\"padding:1px 2px 3px 4px; background-color:#fee; color:#fff\">Hello</span> World</p>')
-  })
-})
 describe('Custom Text and Background color', () => {
   const Component = {
     template: '<p v-highlight="{ word: word, live: live, style: style }">Hello World</p>'
@@ -508,4 +445,114 @@ describe('Custom Text and Background color', () => {
   })
 })
 
+describe('Custom Padding', () => {
+  const Component = {
+    template: '<p v-highlight="{ word: word, live: live, style: style }">Hello World</p>'
+  }
 
+  const localVue = createLocalVue()
+
+  localVue.directive('highlight', vueHighlighter)
+
+  const wrapper = shallow(Component, {
+    localVue,
+    data: {
+      word: 'Hello',
+      live: true,
+      style: {
+        padding: ''
+      }
+    }
+  })
+
+  it('Default padding', () => {
+    expect(wrapper.html()).toBe('<p><span style=\"padding:0px 5px; background-color:#009688; color:#fff\">Hello</span> World</p>')
+  })
+
+  it('Custom padding - 1 value', () => {
+    wrapper.setData({
+      style: {
+        padding: '1px'
+      }
+    })
+    expect(wrapper.html()).toBe('<p><span style=\"padding:1px; background-color:#009688; color:#fff\">Hello</span> World</p>')
+  })
+
+  it('Custom padding - 2 value', () => {
+    wrapper.setData({
+      style: {
+        padding: '1px 2px'
+      }
+    })
+    expect(wrapper.html()).toBe('<p><span style=\"padding:1px 2px; background-color:#009688; color:#fff\">Hello</span> World</p>')
+  })
+
+  it('Custom padding - 3 value', () => {
+    wrapper.setData({
+      style: {
+        padding: '1px 2px 3px'
+      }
+    })
+    expect(wrapper.html()).toBe('<p><span style=\"padding:1px 2px 3px; background-color:#009688; color:#fff\">Hello</span> World</p>')
+  })
+
+  it('Custom padding - 4 value', () => {
+    wrapper.setData({
+      style: {
+        padding: '1px 2px 3px 4px'
+      }
+    })
+    expect(wrapper.html()).toBe('<p><span style=\"padding:1px 2px 3px; background-color:#009688; color:#fff\">Hello</span> World</p>')
+  })
+
+  it('Wrong Padding', () => {
+    wrapper.setData({
+      style: {
+        padding: 'ooo'
+      }
+    })
+    expect(wrapper.html()).toBe('<p><span style=\"padding:0px 5px; background-color:#009688; color:#fff\">Hello</span> World</p>')
+  })
+
+  it('Custom Padding - %', () => {
+    wrapper.setData({
+      style: {
+        padding: '5%'
+      }
+    })
+    expect(wrapper.html()).toBe('<p><span style=\"padding:5%; background-color:#009688; color:#fff\">Hello</span> World</p>')
+  })
+
+  it('Change padding : wrong to correct', () => {
+    wrapper.setData({
+      style: {
+        padding: 'ooo'
+      }
+    })
+    expect(wrapper.html()).toBe('<p><span style=\"padding:0px 5px; background-color:#009688; color:#fff\">Hello</span> World</p>')
+    wrapper.setData({
+      style:{
+        padding: '4rem'
+      }
+    })
+    expect(wrapper.html()).toBe('<p><span style=\"padding:4rem; background-color:#009688; color:#fff\">Hello</span> World</p>')
+  })
+
+  it('Change padding : correct to wrong', () => {
+    wrapper.setData({
+      style: {
+        padding: '4rem'
+      }
+    })
+    expect(wrapper.html()).toBe('<p><span style=\"padding:4rem; background-color:#009688; color:#fff\">Hello</span> World</p>')
+
+    wrapper.setData({
+      style:{
+        padding: 'ooo'
+      }
+    })
+    expect(wrapper.html()).toBe('<p><span style=\"padding:0px 5px; background-color:#009688; color:#fff\">Hello</span> World</p>')
+
+  })
+
+})
